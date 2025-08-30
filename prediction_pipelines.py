1bde329ce1a8e54964af309975988b875d9925e1
Ron D <Ron.Dias@outlook.com>
2025-08-29 21:06:56 -0400

    Prediction 2021-2023 Win Loss Draw

--- prediction_pipelines.py
+++ prediction_pipelines.py
@@ -4,15 +4,16 @@ import pandas as pd
 import seaborn as sns
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+
 # Models
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.svm import SVC
-from sklearn.model_selection import KFold
+
 import structure_data
 
 
@@ -55,121 +56,137 @@ def split_momemtum(d, cat):
     return pd.concat([d, five_games], axis=1)
 
 
-USE_API = False
-SELECTED_TEAM = "Liverpool"
-YEARS = [2021, 2022, 2023]
+if __name__ == "__main__":
+    # get all games for selected team
+    # USE_API = False
+    SELECTED_TEAM = "Liverpool"
+    YEARS = [2021, 2022, 2023]
+    # team_games = structure_data.generate_historic_team_report(
+    #     team_name=SELECTED_TEAM, years=YEARS, use_api=USE_API
+    # )
 
-seasons = []
-for year in YEARS:
-    # get all Premier League games (from API or disk)
-    games = structure_data.get_all_games(year=year, use_api=False)
-    # generate team report
-    seasons.append(
-        structure_data.generate_team_report(season_games=games, team_name=SELECTED_TEAM)
+
+    seasons = []
+    for year in YEARS:
+        # get all Premier League games (from API or disk)
+        games = structure_data.get_all_games(year=year, use_api=False)
+        # generate team report
+        seasons.append(
+            structure_data.generate_team_report(season_games=games, team_name=SELECTED_TEAM)
+        )
+    # merge all seasons
+    df = pd.concat(seasons)
+    # decode prior game performance
+
+    # select features for ML Pipelines
+
+    # pick ML pipelines
+
+    # train/validate models
+
+    # display results
+
+    # drop unknown (`scored`, `conceded`),Opponent (`opponent_name`), Predictions (`result`)
+    # transform Decode (`momentum`,`momentum_opponent`)
+
+    # Decode
+    # momentum_df = split_momemtum(d=df, cat="momentum")
+    # pre_df = split_momemtum(d=momentum_df, cat="momentum_opponent")
+
+    # Drop
+    # Split data
+
+    # X
+    X = df.drop(
+        [
+            "scored",
+            "conceded",
+            "opponent_name",
+            "result",
+            "momentum",
+            "momentum_opponent",
+            "is_home",
+        ],
+        axis=1,
     )
-# merge all seasons
-df = pd.concat(seasons)
-# drop unknown (`scored`, `conceded`),Opponent (`opponent_name`), Predictions (`result`)
-# transform Decode (`momentum`,`momentum_opponent`)
+    # Y
+    res = df["result"]
+    le = LabelEncoder()
+    y = le.fit_transform(res)
 
-# Decode
-momentum_df = split_momemtum(d=df, cat="momentum")
-pre_df = split_momemtum(d=momentum_df, cat="momentum_opponent")
+    # Display X,Y, Correlation Matrix
+    sns.heatmap(X.corr())
 
-# Drop
-# Split data
+    # Prediction
 
-# X
-X = pre_df.drop(
-    [
-        "scored",
-        "conceded",
-        "opponent_name",
-        "result",
-        "momentum",
-        "momentum_opponent",
-        "is_home",
-    ],
-    axis=1,
-)
-# Y
-res = df["result"]
-le = LabelEncoder()
-y = le.fit_transform(res)
-
-# Display X,Y, Correlation Matrix
-sns.heatmap(X.corr())
-
-
-# Prediction
-
-# Split data
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42
-)
-
-
-# Scaling/No Scaling Pipeline
-col_name = X.columns.tolist()
-scale_prior = ColumnTransformer(transformers=[("num", StandardScaler(), col_name)])
-pass_only = ColumnTransformer(transformers=[("num", "passthrough", col_name)])
-
-# Pipelines
-pipelines = {}
-# random forest
-pipelines["random_forest"] = Pipeline(
-    steps=[("prep", scale_prior), ("m", RandomForestClassifier())]
-)
-# logistic regression
-pipelines["logistic_regression"] = Pipeline(
-    steps=[("prep", scale_prior), ("m", LogisticRegression())]
-)
-# support vector
-pipelines["support_vector"] = Pipeline(steps=[("prep", scale_prior), ("m", SVC())])
-# k-nearest-neighbors
-pipelines["k_nearest_neighbors"] = Pipeline(
-    steps=[("prep", scale_prior), ("m", KNeighborsClassifier())]
-)
-# gradient boosting
-pipelines["gradient_boosting"] = Pipeline(
-    steps=[("prep", pass_only), ("m", GradientBoostingClassifier())]
-)
-
-
-
-scores = []
-kFold = KFold(n_splits=10, shuffle=False)
-avg_k_fold = {}
-for name, pipe in pipelines.items():
-    avg_k_fold[name] = []
-for i, (train_index, test_index) in enumerate(kFold.split(X)):
-    X_train, X_test, y_train, y_test = (
-        X.iloc[train_index],
-        X.iloc[test_index],
-        y[train_index],
-        y[test_index],
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.33, random_state=42
     )
-    # Compare
-    results = []
-    fitted = {}
+
+    # Scaling/No Scaling Pipeline
+    col_name = X.columns.tolist()
+    scale_prior = ColumnTransformer(transformers=[("num", StandardScaler(), col_name)])
+    pass_only = ColumnTransformer(transformers=[("num", "passthrough", col_name)])
+
+    # Pipelines
+    pipelines = {}
+    # random forest
+    pipelines["random_forest"] = Pipeline(
+        steps=[("prep", scale_prior), ("m", RandomForestClassifier())]
+    )
+    # logistic regression
+    pipelines["logistic_regression"] = Pipeline(
+        steps=[("prep", scale_prior), ("m", LogisticRegression())]
+    )
+    # support vector
+    pipelines["support_vector"] = Pipeline(steps=[("prep", scale_prior), ("m", SVC())])
+    # k-nearest-neighbors
+    pipelines["k_nearest_neighbors"] = Pipeline(
+        steps=[("prep", scale_prior), ("m", KNeighborsClassifier())]
+    )
+    # gradient boosting
+    pipelines["gradient_boosting"] = Pipeline(
+        steps=[("prep", pass_only), ("m", GradientBoostingClassifier())]
+    )
+
+    scores = []
+    kFold = KFold(n_splits=10, shuffle=False)
+    avg_k_fold = {}
     for name, pipe in pipelines.items():
-        pipe.fit(X_train, y_train)
-        fitted[name] = pipe
-        y_pred = pipe.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
+        avg_k_fold[name] = []
+    for i, (train_index, test_index) in enumerate(kFold.split(X)):
+        X_train, X_test, y_train, y_test = (
+            X.iloc[train_index],
+            X.iloc[test_index],
+            y[train_index],
+            y[test_index],
+        )
+        # Compare
+        results = []
+        fitted = {}
+        for name, pipe in pipelines.items():
+            pipe.fit(X_train, y_train)
+            fitted[name] = pipe
+            y_pred = pipe.predict(X_test)
+            acc = accuracy_score(y_test, y_pred)
 
-        results.append((name, acc))
+            results.append((name, acc))
 
-        avg_k_fold[name].append(acc)
+            avg_k_fold[name].append(acc)
 
-    # Sort by accuracy
-    results_sorted = sorted(results, key=lambda x: x[1], reverse=True)
-    print(f"K-Value:{i}")
-    for name, acc in results_sorted:
-        print(f"{name:22s}  accuracy={acc:.4f}")
-    print("\n\n")
+        # Sort by accuracy
+        results_sorted = sorted(results, key=lambda x: x[1], reverse=True)
+        print(f"K-Value:{i}")
+        for name, acc in results_sorted:
+            print(f"{name:22s}  accuracy={acc:.4f}")
+        print("\n\n")
 
-
-# For each value in dict calculate average
-print(pd.DataFrame(avg_k_fold).mean().sort_values())
-print(X)
+    # For each value in dict calculate average
+    print(pd.DataFrame(avg_k_fold).mean().sort_values())
+    print(X)
+    predict_future=(pd.DataFrame([{"total_score":14,"total_conceded":12,"total_opponent_score":12,"total_opponent_conceded":3}]))
+    for name, pipe in pipelines.items():
+            if name == "support_vector":
+                print(pipe.predict(predict_future))
+                print(le.inverse_transform(pipe.predict(predict_future)))
